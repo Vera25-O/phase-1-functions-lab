6f9cc88161c94389551e7889ee3d339eb216c63a
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-06-07 20:32:30 +0300

    commit tree e3058514d55b956704600279092d13468fa17c66

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+const distanceFromHqInBlocks = (blocks) => Math.abs(42 - blocks);
+const distanceFromHqInFeet = (feet) => 264 * distanceFromHqInBlocks(feet);
+const distanceTravelledInFeet = (start, destination) => Math.abs(start - destination)* 264;
+const calculatesFarePrice = (start, destination) => {
+    let distance = distanceTravelledInFeet(start, destination);
+    if(distance <= 400){
+        return 0;
+    } else if(distance > 400 && distance <= 2000){
+        return (distance - 400)*0.02;
+    } else if(distance > 2000 && distance < 2500){
+        return 25;
+    }else if(distance >= 2500){
+        return 'cannot travel that far'
+    }
+}
+
+    
+
